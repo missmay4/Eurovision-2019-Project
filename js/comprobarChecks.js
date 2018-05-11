ab49098f8426fd775250ccf01175bfebdfd77a57
Unknown <may.eater@gmail.com>
2018-05-11 15:22:53 +0200

    js 3 checks max

---
+++ js/comprobarChecks.js
@@ -0,0 +1,10 @@
+var max_limit = 3; // Max Limit
+$(document).ready(function() {
+  $(".checkear:input:checkbox").each(function(index) {
+    this.checked = (".checkear:input:checkbox" < max_limit);
+  }).change(function() {
+    if ($(".checkear:input:checkbox:checked").length > max_limit) {
+      this.checked = false;
+    }
+  });
+});
