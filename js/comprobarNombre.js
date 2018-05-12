fa07c01b3c5ab9bb52f4f98ac2ad36c0bed30e07
Unknown <may.eater@gmail.com>
2018-05-12 18:40:17 +0200

    peticiones api

--- js/comprobarNombre.js
+++ js/comprobarNombre.js
@@ -1,3 +1,11 @@
+function abreModalNombre() {
+  if (localStorage.getItem('user_name') !== null) {
+    window.location.href = 'votaciones.html';
+  } else {
+    $('#registroModal').modal('show');
+  }
+}
+
 // Validacion nombre usuario
 function onlyLetters(input, event) {
   var letters = /^[A-Za-z]+$/;
@@ -5,5 +13,7 @@ function onlyLetters(input, event) {
     alert('Nombre invalido, por favor introduce un nombre correcto');
     event.preventDefault();
     return false;
+  } else {
+    localStorage.setItem('user_name', $("#name").val());
   }
 }
