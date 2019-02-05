1f02377e2f572cac7f0196af7cd32884843fce99
Mayte <may.eater@gmail.com>
2019-02-05 19:51:17 +0100

    API changes

--- src/API/src/main/java/com/maycosas/eurovision/DBconnection.java
+++ src/API/src/main/java/com/maycosas/eurovision/DBconnection.java
@@ -31,10 +31,6 @@ public class DBconnection {
 		
 		return connection;
 		
-		
-		
 	}
 	
-	
-
 }
--- src/API/src/main/java/com/maycosas/eurovision/entities/Participant.java
+++ src/API/src/main/java/com/maycosas/eurovision/entities/Participant.java
@@ -2,6 +2,32 @@ package com.maycosas.eurovision.entities;
 
 public class Participant {
 	
-	private String name;
 	private Country country;
+	private String name;
+	private String song;
+	
+	public Country getCountry() {
+		return country;
+	}
+	
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSong() {
+		return song;
+	}
+	
+	public void setSong(String song) {
+		this.song = song;
+	} 
+	
 }
